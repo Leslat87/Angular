ecfd0e9b4cd757cf6629ef0950ec44b174ad1a69
Leslat87 <ismaelparada.ip@gmail.com>
2024-05-21 11:15:25 +0200

    modified:   07-formsApp/src/app/auth/auth.module.ts 	modified:   07-formsApp/src/app/auth/pages/register-page/register-page.component.html 	modified:   07-formsApp/src/app/auth/pages/register-page/register-page.component.ts 	modified:   07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.html 	modified:   07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts 	renamed:    07-formsApp/src/app/shared/side-menu/side-menu.component.html -> 07-formsApp/src/app/shared/components/side-menu/side-menu.component.html 	renamed:    07-formsApp/src/app/shared/side-menu/side-menu.component.ts -> 07-formsApp/src/app/shared/components/side-menu/side-menu.component.ts 	new file:   07-formsApp/src/app/shared/service/validators.service.ts 	modified:   07-formsApp/src/app/shared/shared.module.ts 	new file:   07-formsApp/src/app/shared/validators/email-validator.service.ts 	new file:   07-formsApp/src/app/shared/validators/validators.ts

--- 07-formsApp/src/app/shared/shared.module.ts
+++ 07-formsApp/src/app/shared/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { RouterModule } from '@angular/router';
-import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 
 @NgModule({
   declarations: [
-    SideMenuComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
